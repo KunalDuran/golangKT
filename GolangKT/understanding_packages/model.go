d33666f8f66b51f3112a132e668160ebe2d6d3b7
Kunal Duran <kunalduran13@gmail.com>
2024-08-20 16:25:41 +0530

    Delete GolangKT/understanding_packages directory

--- GolangKT/understanding_packages/model.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type Users struct {
-	FirstName string `json:"first_name,omitempty" xml:""`
-	LastName  string `json:"last_name,omitempty"`
-	Email     string `json:"email"`
-	Password  string `json:"-"`
-}
